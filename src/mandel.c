167b285e809dc90c10e619e075b44a30dc0f5615
Nico <nlewicki@student.42heilbronn.de>
2024-10-08 16:41:17 +0200

    added coords to mandel

--- src/mandel.c
+++ src/mandel.c
@@ -6,7 +6,7 @@
 /*   By: nicolewicki <nicolewicki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:38:07 by nicolewicki       #+#    #+#             */
-/*   Updated: 2024/10/08 13:02:17 by nicolewicki      ###   ########.fr       */
+/*   Updated: 2024/10/08 16:36:24 by nicolewicki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,38 @@
 #include "fract_ol.h"
 #include <math.h>
 
+void    init_coords(t_coords *coords)
+{
+    coords->x0 = 0;
+    coords->y0 = 0;
+    coords->x = 0;
+    coords->y = 0;
+    coords->x2 = 0;
+    coords->y2 = 0;
+    coords->i = 0;
+    coords->j = 0;
+}
+
 void calculate_mandelbrot(t_fractol *fractol)
 {
-    double x0 = (fractol->x - WIDTH / 2.0) / fractol->zoom + fractol->offset_x;
-    double y0 = (fractol->y - HEIGHT / 2.0) / fractol->zoom + fractol->offset_y;
-    double x = 0.0;
-    double y = 0.0;
-    double x2 = 0.0;
-    double y2 = 0.0;
-    int i;
+    t_coords coords;
+    init_coords(&coords);
+    coords.x0 = (fractol->x - WIDTH / 2.0) / fractol->zoom + fractol->offset_x;
+    coords.y0 = (fractol->y - HEIGHT / 2.0) / fractol->zoom + fractol->offset_y;
 
-    i = 0;
-    while (i < fractol->max_iterations && (x2 + y2 <= 4.0))
+    coords.i = 0;
+    while (coords.i < fractol->max_iterations && (coords.x2 + coords.y2 <= 4.0))
     {
-        y = 2 * x * y + y0;
-        x = x2 - y2 + x0;
-        x2 = x * x;
-        y2 = y * y;
-        i++;
+        coords.y = 2 * coords.x * coords.y + coords.y0;
+        coords.x = coords.x2 - coords.y2 + coords.x0;
+        coords.x2 = coords.x * coords.x;
+        coords.y2 = coords.y * coords.y;
+        coords.i++;
     }
 
-    double smooth_i = smooth_color(i, fractol->max_iterations, x, y);
+    double smooth_i = smooth_color(coords.i, fractol->max_iterations, coords.x, coords.y);
     
-    if (i == fractol->max_iterations)
+    if (coords.i == fractol->max_iterations)
     {
         mlx_put_pixel(fractol->img, fractol->x, fractol->y, 0x000000);
     }
