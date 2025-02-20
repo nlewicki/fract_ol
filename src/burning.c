6ecbb2875691a6d970e0b540224e8306660a4ae9
Nico <nlewicki@student.42heilbronn.de>
2025-02-20 22:40:01 +0100

    fixed burning ship, and addeed a alternative way of displaying color

--- src/burning.c
+++ src/burning.c
@@ -12,20 +12,23 @@
 
 #include "fract_ol.h"
 
-int	calc_burning_ship(t_fractol *fractol, t_complex *c)
+int calc_burning_ship(t_fractol *fractol, t_complex *c)
 {
-	double		real_temp;
-	t_complex	z;
-	int			i;
+    double      real_temp;
+    t_complex   z;
+    int         i;
 
-	i = -1;
-	z.real = 0;
-	z.imag = 0;
-	while ((z.real * z.real + z.imag * z.imag) < 4 && ++i < fractol->iter)
-	{
-		real_temp = z.real * z.real - z.imag * z.imag + c->real;
-		z.imag = fabs(2 * z.real * z.imag) + c->imag;
-		z.real = (real_temp);
-	}
-	return (i);
+    i = -1;
+    z.real = 0;
+    z.imag = 0;
+    while ((z.real * z.real + z.imag * z.imag) < 4 && ++i < fractol->iter)
+    {
+        z.real = fabs(z.real);
+        z.imag = fabs(z.imag);
+    
+        real_temp = z.real * z.real - z.imag * z.imag + c->real;
+        z.imag = 2 * z.real * z.imag + c->imag;
+        z.real = real_temp;
+    }
+    return (i);
 }
--- src/color.c
+++ src/color.c
@@ -6,7 +6,7 @@
 /*   By: nicolewicki <nicolewicki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 13:04:30 by nlewicki          #+#    #+#             */
-/*   Updated: 2025/02/17 16:19:06 by nicolewicki      ###   ########.fr       */
+/*   Updated: 2025/02/20 22:38:40 by nicolewicki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,40 @@ int calc_color(int iter, int max_iter, int start_color)
 
     return ((int)(color.r_s) << 16 | (int)(color.g_s) << 8 | (int)(color.b_s) | 0xFF);
 }
+
+// int calc_color(int iter, int max_iter, int start_color)
+// {
+//     t_color color;
+//     (void)start_color;
+//     float t;
+
+//     if (iter == max_iter)
+//         return (0x000000FF);  // Black for points in the set
+
+//     t = (float)iter / max_iter;
+    
+//     // Create a fiery gradient
+//     if (t < 0.3)
+//     {
+//         // Dark red to bright red
+//         color.r_s = (t * 3.33) * 255;
+//         color.g_s = 0;
+//         color.b_s = 0;
+//     }
+//     else if (t < 0.6)
+//     {
+//         // Bright red to orange
+//         color.r_s = 255;
+//         color.g_s = ((t - 0.3) * 3.33) * 255;
+//         color.b_s = 0;
+//     }
+//     else
+//     {
+//         // Orange to yellow highlights
+//         color.r_s = 255;
+//         color.g_s = 200;
+//         color.b_s = ((t - 0.6) * 2.5) * 255;
+//     }
+
+//     return ((int)(color.r_s) << 16 | (int)(color.g_s) << 8 | (int)(color.b_s) | 0xFF);
+// }
